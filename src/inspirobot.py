134ce5e8ef1e7b4482d0b960137d492bbbfe93ab
Klemek <klemek@outlook.fr>
2021-06-11 13:10:14 +0200

    handle exceptions in inspirobot

--- src/inspirobot.py
+++ src/inspirobot.py
@@ -2,6 +2,9 @@ import discord
 import requests
 
 async def process(client: discord.client, message: discord.Message, *args: str):
-    async with message.channel.typing():
-        r = requests.get('https://inspirobot.me/api?generate=true')
-        await message.channel.send(r.text)
+    try:
+        async with message.channel.typing():
+            r = requests.get('https://inspirobot.me/api?generate=true')
+            await message.channel.send(r.text)
+    except Exception as e:
+        await message.channel.send(f"Python Exception:\n```{e}```")
