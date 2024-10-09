c99084124cf04bb8bd05f676938e3084d4e51814
Klemek <klemek@outlook.fr>
2024-10-09 23:15:37 +0200

    fix private messages

--- src/caps_alert.py
+++ src/caps_alert.py
@@ -7,10 +7,11 @@ config = load_data("caps_alert", default={})
 
 
 async def process(client: discord.client, message: discord.Message, *args: str):
+    guild_id = message.guild.id if message.guild is not None else message.channel.id
     if len(args) != 2:
         await message.reply("Invalid number of arguments")
     elif args[1].lower() == "off":
-        config[str(message.guild.id)] = None
+        config[str(guild_id)] = None
         await message.add_reaction("✅")
     else:
         try:
@@ -19,17 +20,21 @@ async def process(client: discord.client, message: discord.Message, *args: str):
             await message.add_reaction("❎")
             return
         await message.add_reaction("✅")
-        config[str(message.guild.id)] = args[1]
+        config[str(guild_id)] = args[1]
     save_data("caps_alert", config)
 
 
 async def on_message(message: discord.Message):
-    if (
-        str(message.guild.id) in config
-        and config[str(message.guild.id)] is not None
-        and message.content != None
-        and len(message.content) > 0
-        and message.content == message.content.upper()
-        and message.content != message.content.lower()
-    ):
-        await message.add_reaction(config[str(message.guild.id)])
+    guild_id = message.guild.id if message.guild is not None else message.channel.id
+    try:
+        if (
+            str(guild_id) in config
+            and config[str(guild_id)] is not None
+            and message.content != None
+            and len(message.content) > 0
+            and message.content == message.content.upper()
+            and message.content != message.content.lower()
+        ):
+            await message.add_reaction(config[str(guild_id)])
+    except:
+        pass
--- src/distopia.py
+++ src/distopia.py
@@ -9,14 +9,15 @@ config = load_data("distopia", default={})
 
 
 async def process(client: discord.client, message: discord.Message, *args: str):
+    guild_id = message.guild.id if message.guild is not None else message.channel.id
     if len(args) != 2:
         await message.reply("Invalid number of arguments")
     elif args[1].lower() == "off":
-        config[str(message.guild.id)] = None
+        config[str(guild_id)] = None
         await message.add_reaction("✅")
     else:
         try:
-            config[str(message.guild.id)] = float(args[1])
+            config[str(guild_id)] = float(args[1])
         except:
             await message.add_reaction("❎")
             return
@@ -25,14 +26,15 @@ async def process(client: discord.client, message: discord.Message, *args: str):
 
 
 async def on_message(message: discord.Message):
+    guild_id = message.guild.id if message.guild is not None else message.channel.id
     if (
-        str(message.guild.id) in config
-        and config[str(message.guild.id)] is not None
+        str(guild_id) in config
+        and config[str(guild_id)] is not None
         and len(message.content) > 0
     ):
         matches = re.findall(r"(di|cri)(\w+)", message.content, re.IGNORECASE)
         for a, b in matches:
-            if random.random() < config[str(message.guild.id)]:
+            if random.random() < config[str(guild_id)]:
                 if a.lower() == "di":
                     await message.channel.send(b.capitalize())
                 else:
